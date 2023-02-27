f106378c2b50cc5758979c511cca93cbe98f8ccf
Fenny Rahmayani <126001059+Fennyrahmayani@users.noreply.github.com>
2023-02-27 17:48:18 +0700

    Tugas 2

---
+++ test.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link hred="<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <font face="Lucida sans unicode"</font>
+    <div class="konten">
+        <h1 class="judul"><a href="msib.html" style="text-decoration: indianred;">Curiculum Vitae</a></h1>
+        <p class="nama">Fenny Rahmayani, 27 Februari 2023</p>
+        <img src="FR.jpg" width="600px">
+        <p align="justify">Hi, my name is Fenny Rahmayani, but everyone knows me as Fenny. I'm an undergraduate student of informatics study program of computer science faculty at University of Singaperbangsa Karawang. I have expertise in the IT field, especially in the fields of data analyst, machine learning, and data scientist. I have organizational, time management, planning and problem solving skills. I gained these skills from the training I attended and my internship experience. My opportunities i can work both with the team and individually. I am a hard worker who wants to keep learning and getting new things.
+        </p>
+        <p><b>Experiences</b>
+            <ul>
+                <li>Koordinator Bidang Minat dan Bakat - BEM Fasilkom 2022</li>
+                <li>Asisten Sekretaris Bidang Program dan Pelaporan - Disdikpora Karawang</li>
+                <li>Ketua Pelaksana COMPFAIR 2021 - BEM Fasilkom 2021</li>
+                <li>SPG - BRI </li>
+            </ul>
+        </p>
+        </div>
+        <div class="am">
+            <div class="data">
+                <h2>About Me</h2>
+                <img src="FR.jpg" alt="Photo_Profil" title="Photo_Profil" width="120px">
+                <p>Nama     : Fenny Rahmayani</p>
+                <p>NPM      : 1910631170020</p>
+                <P>Kelas    : Mentor Kak Rizaldy</P>
+            </div>
+        </div>
+</body>
+</html>
